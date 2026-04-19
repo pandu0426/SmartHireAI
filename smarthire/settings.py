4bafc34374630174710531966f4d6399bf5a851b
Mohammad Tayyab <shaiktayyab224@gmail.com>
2026-04-19 17:43:00 +0530

    fixed feature flag

--- smarthire/settings.py
+++ smarthire/settings.py
@@ -142,7 +142,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 
 # Feature flags for safe AI/ML upgrades
-ENABLE_SEMANTIC_MATCHING = os.environ.get('ENABLE_SEMANTIC_MATCHING', 'False') == 'True'
-ENABLE_AI_COACH = os.environ.get('ENABLE_AI_COACH', 'False') == 'True'
-ENABLE_ADVANCED_PARSING = os.environ.get('ENABLE_ADVANCED_PARSING', 'False') == 'True'
-
+ENABLE_SEMANTIC_MATCHING = os.environ.get('ENABLE_SEMANTIC_MATCHING', 'false').lower() == 'true'
+ENABLE_AI_COACH = os.environ.get('ENABLE_AI_COACH', 'false').lower() == 'true'
+ENABLE_ADVANCED_PARSING = os.environ.get('ENABLE_ADVANCED_PARSING', 'false').lower() == 'true'
+
